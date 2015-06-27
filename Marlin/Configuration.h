133546cff4d7f280e01869a51ea40f038b4b19e3
Bracken Dawson <abdawson@gmail.com>
2015-06-27 17:51:28 +0100

    Make thermal runaway less sensitive

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -265,7 +265,7 @@ your extruder heater takes 2 minutes to hit the target on heating.
 // uncomment the 2 defines below:
 
 // Parameters for all extruder heaters
-#define THERMAL_RUNAWAY_PROTECTION_PERIOD 80 //in seconds
+#define THERMAL_RUNAWAY_PROTECTION_PERIOD 120 //in seconds
 #define THERMAL_RUNAWAY_PROTECTION_HYSTERESIS 4 // in degree Celsius
 
 // If you want to enable this feature for your bed heater,
